13ba9c8fb08d86206ec4ad7b3e964495d86e0b7a
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2025-08-10 09:02:39 +0530

    Update register.jsx

--- Register/register.jsx
+++ Register/register.jsx
@@ -20,42 +20,7 @@ import React from 'react';
  <h3 style={{position: 'absolute',left: '400px',top: '170px',fontSize: '25px',fontFamily: 'Garamond'}}>Creat a New Accout</h3>
 
 
- <Card style={{ width: '50rem',
-                   height: '17rem', 
-                   border: 'none',
-                   backgroundColor: 'rgba(100, 100, 100, 0.1)',
-                   borderRadius: '8px',
-                   position: 'absolute', 
-                   top: '200px',
-                   marginLeft: '400px',
-                 
-                   }}>
-      <Card.Body>
-        
-    <h3 style={{position: 'absolute', fontSize: '15px', fontFamily: 'initial', top : '30px',left: '50px'}}>UserName</h3>
-    <input style={{position: 'absolute',width: '350px',height: '35px',top: '55px', border: 'none', borderRadius: '30px',left:'40px'}}
-    onChange={(e) => setName(e.target.value)}></input>
-    <h3 style={{position: 'absolute', fontSize: '15px', fontFamily: 'initial', top: '100px',left: '50px'}}>Email</h3>
-    <input style={{position: 'absolute',width: '350px',height: '35px',top: '125px',border: 'none',borderRadius: '30px',left:'40px'}}
-    onChange={(e) => setEmail(e.target.value)}></input>
-    <h3 style={{position: 'absolute', fontSize: '15px', fontFamily: 'initial', top: '170px',left: '50px'}}>Password</h3>
-    <input type="password"
-           name="password"
-           className='from-control runded-0'
-    style={{position: 'absolute',width: '350px',height: '35px',top: '195px',border: 'none',borderRadius: '30px',left:'40px'}}
-    onChange={(e) => setPassword(e.target.value)}></input>
-   
-   
-    
 
-    <button onClick={handleSubmit}  style={{position: 'absolute', fontSize: '20px', fontFamily: 'initial', top: '70px',left: '500px',width: '250px',height: '40px', backgroundColor: '#7272FF',color: 'white',border: 'none',borderRadius: '50px'}}>Register</button>
-    <h3 style={{position: 'absolute', fontSize: '10px', fontFamily: 'initial', top: '130px',left: '570px'}}>Already Have a Account</h3>
-    <button style={{position: 'absolute', fontSize: '20px', fontFamily: 'initial', top: '170px',left: '500px',width: '250px',height: '40px', backgroundColor: '#E84B4F',color: 'white',border: 'none',borderRadius: '50px'}}>Login</button>
-    
-
-   
-      </Card.Body>
-    </Card> 
       
      
 
@@ -82,6 +47,7 @@ import React from 'react';
 }
 
 export default register;
+
 
 
 
